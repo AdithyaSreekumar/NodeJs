8cbcf4029c5132462e4642ef4f8cd448fab1ca0e
BingVader <raothomas1@gmail.com>
2025-03-03 12:36:57 +0530

    Learning Express

--- app.js
+++ app.js
@@ -6,6 +6,53 @@ app.get('/',(req,res)=>{
     res.send('Hello Express!'); 
 });
 
+app.get('/getname', async(req,res)=>{
+    try {
+        var { name } = req.query;
+        if (!name){
+            res.status(400).json({
+                status: false,
+                status_code:400,
+                message: "Please provide your name",
+                data: null
+            });
+            return;
+        }
+            console.log("name",name);
+            res.status(200).json({
+                status: true,
+                status_code:200,
+                message: "Request successful",
+                data: `Hai my name is ${name}.`
+            });
+    } catch (error) {
+       console.error(error);
+       res.status(500).json({
+        status: false,
+        status_code:500,
+        message: "Something went wrong",
+        data: null
+    });
+    }
+
+    var { name } = req.query;
+    if (!name){
+        res.status(400).json({
+            status: false,
+            status_code:400,
+            message: "Please provide your name",
+            data: null
+        });
+
+        res.status(500).json({
+            status: false,
+            status_code:400,
+            message: "Please provide your name",
+            data: null
+        });
+    }
+});
+
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}`);
 });
