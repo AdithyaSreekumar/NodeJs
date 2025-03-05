3cb130ceb55b03c4a5ba524442c7db75c146ca72
BingVader <raothomas1@gmail.com>
2025-03-05 10:31:35 +0530

    Created Books and Employee Schema

---
+++ model/Books.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const books = new mongoose.Schema({
+    title: { type:String, required:true },
+    author: { type:String, required:true },
+    genre: { type:String, required:true },
+    year: { type:Number, required:true },
+    price: { type:Number, required:true }
+})
+
+const booksModel = mongoose.model("booksModel",books)
+
+module.exports = booksModel;
