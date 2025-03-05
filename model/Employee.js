314665fe129c731cccde0b7d8bd958784f711d70
BingVader <raothomas1@gmail.com>
2025-03-05 17:01:18 +0530

    Employee CRUD halfway

--- model/Employee.js
+++ model/Employee.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const userModel = require('./userdata');
 const employeeSchema = new mongoose.Schema({
-    employeeId:{type:String,required:true,unique:true},
+    // employeeId:{type:String,required:true,unique:true},
     quali:{type:String,required:true},
-    salary:{type:String,required:true}    
+    salary:{type:String,required:true},
+    empID :{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'userModel'
+    },
+    name :{
+        type: mongoose.Schema.Types.String,
+        ref : 'userModel'
+    }
 })
 
 const empModel = mongoose.model("empModel",employeeSchema)
